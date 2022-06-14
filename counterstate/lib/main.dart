4c1954acd9be5d272bdd17adbfa6a251e5bf156e
Bryan Dixon <javawolfpack@gmail.com>
2022-06-14 18:19:43 -0400

    Elevated Buttons

--- counterstate/lib/main.dart
+++ counterstate/lib/main.dart
@@ -22,9 +22,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CINS467 Awesome App'),
     );
   }
 }
@@ -52,12 +52,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
     });
   }
 
@@ -94,22 +95,36 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'The count is:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          children: getBody(),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add_a_photo),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  List<Widget> getBody() {
+    List<Widget> list = List.empty(growable: true);
+    list.add(const Text(
+      'The count is:',
+    ));
+    list.add(Text(
+      '$_counter',
+      style: Theme.of(context).textTheme.headline4,
+    ));
+    list.add(Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      ElevatedButton(
+        onPressed: _counter > 0 ? _decrementCounter : null,
+        child: const Icon(Icons.remove),
+      ),
+      ElevatedButton(
+        onPressed: _counter < 10 ? _incrementCounter : null,
+        child: const Icon(Icons.add),
+      )
+    ]));
+    return list;
   }
 }
