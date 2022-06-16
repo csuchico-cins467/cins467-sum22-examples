06bb5d7eeb6d3b1c06c331fff0fd078a15da29b8
Bryan Dixon <javawolfpack@gmail.com>
2022-06-16 18:20:55 -0400

    REST, ListView, Images, URL

---
+++ recipeapp/lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<List<dynamic>> recipes;
+
+  Future<List<dynamic>> getRecipes() async {
+    var url = Uri.https('rest.bryancdixon.com', '/food/');
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      return jsonResponse['recipes'];
+    } else {
+      if (kDebugMode) {
+        print("Request Failed with status: ${response.statusCode}.");
+      }
+      return List.empty();
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    recipes = getRecipes();
+  }
+
+  void _launchUrl(Uri _url) async {
+    if (!await launchUrl(_url)) throw 'Could not launch $_url';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder<List>(
+            future: recipes,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return const CircularProgressIndicator();
+                default:
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                            onTap: () {
+                              _launchUrl(
+                                  Uri.parse(snapshot.data![index]['url']));
+                            },
+                            child: Card(
+                              child: Column(children: [
+                                ListTile(
+                                  title: Text(snapshot.data![index]['title']),
+                                ),
+                                Image.network(
+                                    snapshot.data![index]['photo_url'])
+                              ]),
+                            ));
+                      },
+                    );
+                  }
+              }
+            })
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
+  }
+}
