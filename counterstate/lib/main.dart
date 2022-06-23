9e295a5f9e0be43d6bd88e9a617fb2d61f76d9d4
Bryan Dixon <javawolfpack@gmail.com>
2022-06-23 15:13:57 -0400

    Location Services

--- counterstate/lib/main.dart
+++ counterstate/lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:counterstate/storage.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,6 +58,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Future<int> _counter;
+  late Future<Position> _position;
 
   Future<void> _incrementCounter() async {
     int counter = await widget.storage.readCounter();
@@ -62,6 +67,48 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _counter = widget.storage.readCounter();
     });
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
   }
 
   Future<void> _decrementCounter() async {
@@ -77,6 +124,22 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     _counter = widget.storage.readCounter();
+    _position = _determinePosition();
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position? position) {
+      if (kDebugMode) {
+        if (position != null) {
+          //calculate distance from target 39.72545, -121.80228
+        }
+        print(position == null
+            ? 'Unknown'
+            : '${position.latitude.toString()}, ${position.longitude.toString()}');
+      }
+    });
   }
 
   @override
@@ -125,6 +188,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Widget> getBody() {
     List<Widget> list = List.empty(growable: true);
+    list.add(FutureBuilder<Position>(
+        future: _position,
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            default:
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                return Text(
+                    '${snapshot.data!.latitude}, ${snapshot.data!.longitude}, ${snapshot.data!.accuracy}');
+              }
+          }
+        }));
     list.add(const Text(
       'The count is:',
     ));
--- counterstate/macos/Flutter/GeneratedPluginRegistrant.swift
+++ counterstate/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,10 @@ import Foundation
 
 import cloud_firestore
 import firebase_core
-import sqflite
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
