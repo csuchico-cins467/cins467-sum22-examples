928470422a45d72b878f29bfaa50bcde76f740e0
Bryan Dixon <javawolfpack@gmail.com>
2022-06-29 17:12:45 -0400

    Firebase storages

--- counterstate/lib/main.dart
+++ counterstate/lib/main.dart
@@ -3,12 +3,11 @@ import 'dart:io';
 import 'dart:core';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:counterstate/firebase_options.dart';
 import 'package:counterstate/storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import 'package:geolocator/geolocator.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,14 +33,13 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
-      home: MyHomePage(title: 'CINS467 Awesome App', storage: CounterStorage()),
+      home: MyHomePage(title: 'CINS467 Awesome App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title, required this.storage})
-      : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -53,102 +51,15 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
-  final CounterStorage storage;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<int> _counter;
-  late Future<Position> _position;
-  File? _image;
-
-  Future<void> _incrementCounter() async {
-    int counter = await widget.storage.readCounter();
-    counter += 1;
-    await widget.storage.writeCounter(counter);
-    setState(() {
-      _counter = widget.storage.readCounter();
-    });
-  }
-
-  void _getImage() async {
-    final ImagePicker _picker = ImagePicker();
-    // Pick an image
-    final XFile? pickedImage =
-        await _picker.pickImage(source: ImageSource.camera);
-    setState(() {
-      if (pickedImage != null) {
-        _image = File(pickedImage.path);
-      } else {
-        if (kDebugMode) {
-          print('No image selected.');
-        }
-      }
-    });
-  }
-
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-  }
-
-  Future<void> _decrementCounter() async {
-    int counter = await widget.storage.readCounter();
-    counter -= 1;
-    await widget.storage.writeCounter(counter);
-    setState(() {
-      _counter = widget.storage.readCounter();
-    });
-  }
-
   @override
   void initState() {
     super.initState();
-    _counter = widget.storage.readCounter();
-    _position = _determinePosition();
-    const LocationSettings locationSettings = LocationSettings(
-      accuracy: LocationAccuracy.high,
-      distanceFilter: 100,
-    );
   }
 
   @override
@@ -159,135 +70,73 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: SingleChildScrollView(
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: getBody(),
-        ),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getImage,
-        tooltip: 'Add a photo',
-        child: const Icon(Icons.add_a_photo),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform),
+        builder: ((context, snapshot) {
+          if (snapshot.hasError) {
+            if (kDebugMode) {
+              print(snapshot.error);
+            }
+            return Text("Something Went Wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                // Here we take the value from the MyHomePage object that was created by
+                // the App.build method, and use it to set our appbar title.
+                title: Text(widget.title),
+              ),
+              body: Column(
+                children: [getBody()],
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: ((context) =>
+                              SecondScreen(title: "Get a photo"))));
+                },
+                tooltip: 'Add a photo',
+                child: const Icon(Icons.add_a_photo),
+              ), // This trailing comma makes auto-formatting nicer for build methods.
+            );
+          }
+          return CircularProgressIndicator();
+        }));
   }
 
-  List<Widget> getBody() {
-    List<Widget> list = List.empty(growable: true);
-    list.add(Text(DateTime.now().toIso8601String()));
-    _image == null
-        ? list.add(
-            Container(
-              margin: const EdgeInsets.all(10.0),
-              child: Placeholder(
-                child: Image.network(
-                    "https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"),
-              ),
-              width: 200.0,
-              height: 200.0,
-            ),
-          )
-        : list.add(Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Image.file(_image!),
-            width: 200.0,
-            height: 200.0,
-          ));
-    _image == null
-        ? list.add(
-            Placeholder(
-              child: Image.network(
-                  "https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"),
-            ),
-          )
-        : list.add(Image.file(_image!));
-    list.add(FutureBuilder<Position>(
-        future: _position,
-        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const CircularProgressIndicator();
-            default:
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return Text(
-                    '${snapshot.data!.latitude}, ${snapshot.data!.longitude}, ${snapshot.data!.accuracy}');
-              }
-          }
-        }));
-    list.add(const Text(
-      'The count is:',
-    ));
-    // list.add(Text(
-    //   '$_counter',
-    //   style: Theme.of(context).textTheme.headline4,
-    // ));
-    if (widget.storage.isInitalized) {
-      list.add(StreamBuilder(
-          stream: FirebaseFirestore.instance.collection("example").snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) {
-              return Text(snapshot.error.toString());
-            }
-            if (!snapshot.hasData) return CircularProgressIndicator();
-            return Text(
-              '${snapshot.data!.docs[0]["count"]}',
-              style: Theme.of(context).textTheme.headline4,
-            );
-          }));
+  Widget photoWidget(AsyncSnapshot<QuerySnapshot> snapshot, int index) {
+    try {
+      return Column(children: [
+        ListTile(title: Text(snapshot.data!.docs[index]['title'])),
+        Image.network(
+          snapshot.data!.docs[index]['downloadURL'],
+          height: 150,
+        )
+      ]);
+    } catch (e) {
+      return ListTile(title: Text("Error: $e"));
     }
-    list.add(FutureBuilder<int>(
-        future: _counter,
-        builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const CircularProgressIndicator();
-            default:
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else {
-                return Text(
-                  '${snapshot.data}',
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              }
+  }
+
+  Widget getBody() {
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("photos").snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
           }
-        }));
-    list.add(Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      ElevatedButton(
-        onPressed: _decrementCounter,
-        child: const Icon(Icons.remove),
-      ),
-      ElevatedButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      )
-    ]));
-    return list;
+          if (!snapshot.hasData) return const Text("Loading Photos");
+          return Expanded(
+              child: Scrollbar(
+                  child: ListView.builder(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return photoWidget(snapshot, index);
+                      })));
+        });
   }
 }
--- counterstate/lib/storage.dart
+++ counterstate/lib/storage.dart
@@ -1,14 +1,31 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:counterstate/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
-class CounterStorage {
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  File? _image;
   bool _initialized = false;
+  FirebaseApp? app;
+
   Future<void> initializeDefault() async {
-    FirebaseApp app = await Firebase.initializeApp(
+    app = await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
     _initialized = true;
     if (kDebugMode) {
@@ -16,43 +33,93 @@ class CounterStorage {
     }
   }
 
-  bool get isInitalized => _initialized;
-
-  Future<bool> writeCounter(int counter) async {
-    if (!_initialized) {
-      await initializeDefault();
-    }
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    firestore.collection("example").doc("cins467").set({
-      'count': counter,
-    }).then(((value) {
-      if (kDebugMode) {
-        print("Count Added");
-      }
-      return true;
-    })).catchError((error) {
-      if (kDebugMode) {
-        print(error);
-      }
-      return false;
-    });
-    return false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initializeDefault();
   }
 
-  Future<int> readCounter() async {
+  void _getImage() async {
+    final ImagePicker _picker = ImagePicker();
+    // Pick an image
+    final XFile? pickedImage =
+        await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        if (kDebugMode) {
+          print('No image selected.');
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _image == null
+                ? Text('No image selected.')
+                : Image.file(_image!, width: 300),
+            ElevatedButton(
+                onPressed: () {
+                  _upload();
+                },
+                child: const Text("Submit", style: TextStyle(fontSize: 20))),
+          ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _getImage,
+        tooltip: 'Add a photo',
+        child: const Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+
+  Future<String> _uploadFile(filename) async {
     if (!_initialized) {
       await initializeDefault();
     }
-    FirebaseFirestore firestore = FirebaseFirestore.instance;
-    DocumentSnapshot ds =
-        await firestore.collection("example").doc("cins467").get();
-    Map<String, dynamic>? data = (ds.data() as Map<String, dynamic>);
-    if (data == null) {
-      await writeCounter(0);
-    } else if (data.containsKey("count")) {
-      return data["count"];
+    final Reference ref = FirebaseStorage.instance.ref().child('$filename.jpg');
+    final SettableMetadata metadata =
+        SettableMetadata(contentType: 'image/jpeg', contentLanguage: 'en');
+    final UploadTask uploadTask = ref.putFile(_image!, metadata);
+    final downloadURL = await (await uploadTask).ref.getDownloadURL();
+    if (kDebugMode) {
+      print(downloadURL.toString());
     }
-    await writeCounter(0);
-    return 0;
+    return downloadURL.toString();
+  }
+
+  void _upload() async {
+    if (!_initialized) {
+      await initializeDefault();
+    }
+    if (_image != null) {
+      var uuid = Uuid();
+      final String uid = uuid.v4();
+      if (kDebugMode) {
+        print(uid);
+      }
+      final String downloadURL = await _uploadFile(uid);
+      await _addItem(downloadURL, uid);
+      Navigator.pop(context);
+    }
+  }
+
+  Future<void> _addItem(String downloadURL, String title) async {
+    if (!_initialized) {
+      await initializeDefault();
+    }
+    await FirebaseFirestore.instance.collection('photos').add(<String, dynamic>{
+      'downloadURL': downloadURL,
+      'title': title,
+      'geopoint': GeoPoint(4.5, 3.5),
+      'timestamp': DateTime.now()
+    });
   }
 }
